84ae8437b2a9f690a8fed216c873d3501736d8ea
Pedro Rafael Santos <psantos@eneida.io>
2023-08-08 17:26:09 +0100

    ds

---
+++ AutoMoreira.Persistence/Interfaces/Repositories/IVehicleImageRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoMoreira.Persistence.Interfaces.Repositories
+{
+    public interface IVehicleImageRepository
+    {
+        Task<VehicleImage[]> GetAllVehicleImagesAsync();
+        Task<VehicleImage> GetVehicleImageByIdAsync(int vehicleImageId);
+        Task<VehicleImage[]> GetVehicleImagesByVehicleIdAsync(int vehicleId);
+    }
+}
---
+++ AutoMoreira.Persistence/Repositories/VehicleRepositoryImage.cs
@@ -0,0 +1,53 @@
+﻿namespace AutoMoreira.Persistence.Repositories
+{
+    public class VehicleImageRepository : IVehicleImageRepository
+    {
+        private readonly AppDbContext _context;
+        public VehicleImageRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<VehicleImage[]> GetAllAsync()
+        {
+            IQueryable<VehicleImage> query = _context.VehicleImages;
+
+            query = query
+                .AsNoTracking()
+                .OrderBy(v => v.Id);
+
+            return await query.ToArrayAsync();
+        }
+
+        public Task<VehicleImage[]> GetAllVehicleImagesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<VehicleImage> GetVehicleImageByIdAsync(int vehicleImageId)
+        {
+            IQueryable<VehicleImage> query = _context.VehicleImages;
+
+
+            query = query
+                .AsNoTracking()
+                .OrderBy(p => p.Id)
+                .Where(p => p.Id == vehicleImageId);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<VehicleImage[]> GetVehicleImagesByVehicleIdAsync(int vehicleId)
+        {
+            IQueryable<VehicleImage> query = _context.VehicleImages;
+
+            query = query
+                .AsNoTracking()
+                .OrderBy(v => v.Id)
+                .Where(p => p.VehicleId == vehicleId);
+
+            return await query.ToArrayAsync();
+        }
+    }
+}
