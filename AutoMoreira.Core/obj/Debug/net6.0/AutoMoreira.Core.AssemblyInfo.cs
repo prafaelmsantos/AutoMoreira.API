95c1082ede5d6685394538eabfe6723a60671ded
Pedro Rafael Santos <psantos@eneida.io>
2023-11-14 22:01:58 +0000

    add docker suport

--- AutoMoreira.Core/obj/Debug/net6.0/AutoMoreira.Core.AssemblyInfo.cs
+++ AutoMoreira.Core/obj/Debug/net6.0/AutoMoreira.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMoreira.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b02e8025355bd3d10e2fb3535a0ee443b155590")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMoreira.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMoreira.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
