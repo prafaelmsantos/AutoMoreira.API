2342d080ae67192fc12090fe8f387b3aff6411d8
Pedro Rafael Santos <psantos@eneida.io>
2024-02-10 02:28:04 +0000

    zzx

--- AutoMoreira.Persistence/obj/Debug/net6.0/AutoMoreira.Persistence.AssemblyInfo.cs
+++ AutoMoreira.Persistence/obj/Debug/net6.0/AutoMoreira.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMoreira.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75385a08ecd53cd6c2e38f5bf8b26dab26cfc45d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e98c0ea7b57bed82a7d618a301154babc948e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMoreira.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMoreira.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
